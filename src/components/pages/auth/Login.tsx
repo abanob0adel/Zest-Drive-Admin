80d5c40943202585d9c9f74dc8e3094c8823b923
Abanob Adel <me@abanob.co>
2025-10-15 19:41:25 +0300

    Finish Login Form

--- src/components/pages/auth/Login.tsx
+++ src/components/pages/auth/Login.tsx
@@ -1,10 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function Login() {
   return (
     <>
       <div className="container">
         <div className="min-h-screen flex items-center justify-center">
-          <form className="p-6 border rounded-2xl lg:min-w-1/2">
-            <h4 className="text-center text-2xl font-semibold">سجل دخولك</h4>
+          <form className="p-6 border rounded-2xl lg:min-w-1/2 min-w-full">
+            <div className="grid grid-cols-1 gap-4">
+              <h4 className="text-center text-2xl font-semibold">سجل دخولك</h4>
+              <Input placeholder="بريدك الالكتروني" type="email" />
+              <Input placeholder="*********" type="password" />
+              <Button className="w-full">سجل دخولك الان</Button>
+            </div>
           </form>
         </div>
       </div>
