6697c522c901c8d48b94c250151065e003e51e37
Abanob Adel <me@abanob.co>
2025-10-30 10:20:48 +0300

    Fix Edit Car

--- src/components/pages/dashboard/cars/edit/request.ts
+++ src/components/pages/dashboard/cars/edit/request.ts
@@ -12,7 +12,8 @@ export const getCarDetails = async (
   };
 };
 
-export const editCarRequest = async (slug: string, data: FormData) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const editCarRequest = async (slug: string, data: any) => {
   const res = await axios.put(`cars/${slug}`, data, {
     headers: { "Content-Type": "application/json" },
   });
